313f53cb994eeaf1005992b6a43b231dc17bb5c3
imyogeshwaran <itsyogeshwaran11@gmail.com>
2025-11-08 20:06:51 +0530

    after removin' unwanted file

--- server/server.js
+++ server/server.js
@@ -49,6 +49,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the public directory
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+
 // Serve uploaded files from /uploads
 const uploadsPath = path.join(__dirname, 'public', 'uploads');
 console.log('Serving uploads from:', uploadsPath);
